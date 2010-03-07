472f1453195fbff24c6c9241cd7207b668ad9ffe
Paul Jakma <paul@jakma.org>
2010-03-07 22:32:39 +0000

    AbstractPositionableNode: some obvious, basic implementation for PositionableNode

---
+++ src/org/nongnu/multigraph/layout/AbstractPositionableNode.java
@@ -0,0 +1,45 @@
+package org.nongnu.multigraph.layout;
+
+public abstract class AbstractPositionableNode
+                implements PositionableNode {
+  Vector2D position = new Vector2D (0,0);
+  Vector2D velocity = new Vector2D (0,0);
+  float mass = 1;
+  float size = 1;
+  
+  @Override
+  public float getMass () {
+    return mass;
+  }
+  
+  @Override
+  public Vector2D getPosition () {
+    return position;
+  }
+  
+  @Override
+  public Vector2D getVelocity () {
+    return velocity;
+  }
+  
+  @Override
+  public void setMass (float m) {
+    mass = m;
+  }
+  
+  @Override
+  public float getSize () {
+    return size;
+  }
+  
+  @Override
+  public void setSize (float s) {
+    this.size = s;
+  }
+  
+  @Override
+  public boolean isMovable () {
+    return true;
+  }
+
+}
