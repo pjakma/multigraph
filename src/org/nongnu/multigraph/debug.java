83f7527fcf789c953e0a6d56f9041668dc669f91
Paul Jakma <paul@jamaica.dcs.gla.ac.uk>
2011-02-14 13:30:24 +0000

    debug: Add support for ring-buffer.
    * debug: Use java.util.logging MemoryHandler to add support for logging to a
      ring-buffer, triggering when required.  E.g.  to allow last X lines to be
      logged if an error occurs.
      TODO: convert over to standard java.util.logging debug levels, and get
      rid of hacky conversion between own levels and java levels.

--- src/org/nongnu/multigraph/debug.java
+++ src/org/nongnu/multigraph/debug.java
@@ -15,18 +15,124 @@
  */
 package org.nongnu.multigraph;
 
-import java.io.*;
+import java.io.PrintStream;
+import java.util.Formatter;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.MemoryHandler;
 
+/**
+ * Custom debug class with support for various print statements, different debug levels
+ * and a ring-buffer to store debug message history that is only printed out if
+ * a message of at least a certain level (e.g. an error) is logged.
+ * 
+ * This is intended as a convenience wrapper around java.util.logging.
+ * 
+ * @author paul.
+ */
 public class debug {
-  // debug level, from 0 (none) on up.
-  public static PrintStream out = System.out;
-  public static levels level = levels.NONE;
   public enum levels {
-    NONE,
-    ERROR,
-    WARNING,
-    INFO,
-    DEBUG,
+    NONE(Level.OFF),
+    ERROR(Level.SEVERE),
+    WARNING(Level.WARNING),
+    INFO(Level.INFO),
+    DEBUG(Level.FINEST);
+    private final Level level;
+    private levels (Level l) { level = l; }
+    Level toLevel () { return level; }
+    static levels tolevel (Level l) {
+      for (levels ls : levels.values ())
+        if (ls.ordinal () == l.intValue ())
+          return ls;
+      return levels.NONE;
+    }
+  }
+  
+  public static PrintStream out = System.out;
+  private static levels level = levels.NONE;
+  private static java.util.logging.Formatter fmter = new java.util.logging.Formatter () {
+    @Override
+    public String format (LogRecord record) {
+      return record.getLevel () + ": " + record.getMessage ();
+    }
+    
+  };
+  
+  private static ConsoleHandler ch = new ConsoleHandler ();
+  private static MemoryHandler mh
+    = new MemoryHandler (ch, 4096000, level.level);
+  public static Logger logger = Logger.getAnonymousLogger ();
+  private static boolean once = true;
+  
+  /**
+   * The current debug level. Messages below this level will not be captured.
+   * @see #level.
+   * @return Current debug.level.
+   */
+  public static levels level () {
+    return level;
+  }
+  /**
+   * Set the current debug level. Messages below this level will not be captured.
+   * @see #level.
+   * @param l The debug.level to set.
+   */
+  public static void level (levels l) {
+    level = l;
+    mh.setPushLevel (l.level);
+  }
+  /**
+   * Set the current debug level. Messages below this level will not be captured.
+   * @see #level.
+   * @param l String representation of debug.level to set.
+   */
+  public static void level (String l) {
+    level(levels.valueOf (l.toUpperCase ()));
+  }
+  /**
+   * The current push-level. Debug messages that have been captured are buffered
+   * in a ring-buffer in memory and not pushed out until a message of at least 
+   * this level is seen. This defaults to the current debug level, and is reset
+   * accordingly whenever the debug level is reset - the push-level must be
+   * set after the debug level if a different level is desired. @see #level(levels).
+   * <p>
+   * E.g. Setting the debug-level to "debug", but the push-level to "error" would
+   * print out the last X lines of "debug" output, but only if a "error" level message
+   * was received.
+   * 
+   * @return
+   */
+  public static levels pushlevel () {
+    return levels.tolevel (mh.getPushLevel ());
+  }
+  /**
+   * Set the current push-level. @see #pushlevel and @see #level.
+   * @param l the {@link #level} to set the push-level to.
+   */
+  public static void pushlevel (levels l) {
+    mh.setPushLevel (l.level);
+  }
+  /**
+   * Set the current push-level. @see #pushlevel and @see #level.
+   * @param l String form of the {@link #level} to set the push-level to.
+   */
+  public static void pushlevel (String l) {
+    mh.setPushLevel (levels.valueOf (l.toUpperCase ()).level);
+  }
+  
+  /**
+   * Change the size of the ring-buffer used to buffer log records
+   * when the push-level is higher than the debug level. This
+   * defaults to 4096000 (i.e. quite a lot). Setting this parameter
+   * destroys any existing log messages.
+   * @param s
+   */
+  public static void buffersize (int s) {
+    MemoryHandler newmh = new MemoryHandler (ch, s, mh.getPushLevel ());
+    newmh.setLevel (mh.getLevel ());
+    mh = newmh;
   }
   
   public static boolean applies (levels d) {
@@ -34,26 +140,42 @@ public class debug {
       ? false
       : true;
   }
+  
   public static boolean applies () {
 	  return applies (levels.DEBUG);
+  }
+  
+  public static void printf (levels d, String s) {
+    if (!applies (d))
+      return;
+    
+    if (once) {
+      ch.setFormatter (fmter);
+      logger.setLevel (levels.DEBUG.level);
+      ch.setLevel (debug.levels.DEBUG.level);
+      logger.addHandler (mh);
+      once = false;
+    }
+    
+    logger.log (d.level, s);
   }
   
   public static void printf (levels d, String format, Object... args) {
     if (!applies (d))
       return;
-    out.printf (format, args);
-  }
-  public static void printf (levels d, String s) {
-    if (!applies (d))
-      return;
-    out.printf (s);
+    
+    StringBuilder sb;
+    
+    new Formatter ((sb = new StringBuilder ())).format (format, args);
+    
+    printf (d, sb.toString ());
   }
 
   public static void println (levels d, String s) {
     if (!applies (d))
       return;
-      
-    out.println (s);
+    
+    printf (d, s + "\n");
   }
 
   // default debug level versions
@@ -63,10 +185,7 @@ public class debug {
   public static void printf (String s) {
     printf (levels.DEBUG, s);
   }
-
   public static void println (String s) {
     println (levels.DEBUG, s);
   }
-
-
 }
