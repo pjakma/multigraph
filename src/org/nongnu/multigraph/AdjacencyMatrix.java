44d8edbc92944d6424670543d1510b3eb24cc613
Paul Jakma <paul@jamaica.dcs.gla.ac.uk>
2011-02-28 14:41:24 +0000

    AdjacencyMatrix: Add a simple parser for Matlab/Octave style adjacency-list
    * AdjacencyMatrix: (parse) construct a graph from a Matlab/Octave style
      adjacency list description.

--- src/org/nongnu/multigraph/AdjacencyMatrix.java
+++ src/org/nongnu/multigraph/AdjacencyMatrix.java
@@ -1,6 +1,10 @@
 package org.nongnu.multigraph;
 
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 
 /**
  * Output an adjacency matrix of a graph, in a form suitable for MatLab/Octave.
@@ -39,5 +43,31 @@ public class AdjacencyMatrix {
       }
     }
     out.print ("]\n");
+  }
+  
+  public static <N,E> void parse (InputStream in, Graph<N,E> graph,
+                                  NodeLabeler<N,E> nl, EdgeLabeler<N,E> el) {
+    Scanner sc = new Scanner (in);
+    Pattern startp = Pattern.compile ("\\[\\s*");
+    
+    while ((null == sc.findInLine (startp))) sc.nextLine ();
+    sc.nextLine ();
+    
+    String t;
+    while (!(t = sc.next ()).equals ("]")) {
+      N from = nl.getNode (t);
+      N to   = nl.getNode (sc.next ());
+      int w = Integer.parseInt (sc.next ());
+      
+      debug.printf ("setting: %s -> %s (%d)\n", from, to, w);
+      
+      try {
+        graph.set (from, to, el.getLabel (from, to), w);
+      } catch (UnsupportedOperationException e) {
+        debug.printf ("Couldn't not create edge %s -> %s (%d):\n%s\n",
+                     from, to, w, e);
+      }
+    }
+    sc.close ();
   }
 }
