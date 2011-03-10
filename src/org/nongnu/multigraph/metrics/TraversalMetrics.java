6c94129d67b148aaacf687f8352781199dae3e50
Paul Jakma <paul@jamaica.dcs.gla.ac.uk>
2011-03-10 16:58:15 +0000

    TraversalMetrics: Add Floyd-Warshal all-pairs shortest-path algorithm
    * TraversalMetrics: (FloydWarshal) Use the Floyd-Warshal algorithm to
      calculate the shortest-paths for all pairs of nodes in the given graph,
      returning the results in a "dmap" distnace map convenience object.
      (FWdist) helper for FW, calculate distance between two 2 nodes, taking
      existing results into account.
      (FWdplus) helper to handle addition of distances, with absorption to
      max-distance.
      (stats) calculate a number of statistics for paths in the given graph,
      returning them as a labelled map.
    * dmap: new class to track the distances between nodes. Though O(1), it's
      still somewhat heavyweight for dense useage (e.g. as FW uses it).

--- src/org/nongnu/multigraph/metrics/TraversalMetrics.java
+++ src/org/nongnu/multigraph/metrics/TraversalMetrics.java
@@ -1,6 +1,12 @@
 package org.nongnu.multigraph.metrics;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.nongnu.multigraph.Edge;
 import org.nongnu.multigraph.Graph;
+import org.nongnu.multigraph.debug;
 
 /**
  * Return 
@@ -28,7 +34,9 @@ public class TraversalMetrics {
   
   /**
    * Traverse the Graph and create a histogram of the normalised distribution
-   * of nodal out-degree. I.e. the probability of a given degree, for that degree.
+   * of nodal out-degree. I.e. the probability of a given degree, for that 
+   * degree.
+   * 
    * @param <N> The type of the nodes in the graph.
    * @param <E> The type of the edges in the graph.
    * @param graph The graph to traverse
@@ -78,5 +86,98 @@ public class TraversalMetrics {
     for (N node : graph)
       count += graph.edge_outdegree (node);
     return count / (graph.is_directed () ? 1 : 2);
+  }
+  
+  private static <N,E> int FWdist (Graph<N,E> graph, dmap<N> dmap,
+                                   N from, N to) {
+    Collection<Edge<N,E>> edges;
+    int w;
+    
+    if (from == to)
+      return 0;
+    
+    w = dmap.dist (from, to);
+    
+    if (w < Integer.MAX_VALUE)
+      return w;
+    
+    if ((edges = graph.edges (from, to)).size () > 0) {
+      int min = Integer.MAX_VALUE;
+      for (Edge<N,E> e : edges)
+        min = Math.min (min, e.weight ());
+      return min;
+    }
+    
+    return Integer.MAX_VALUE;
+  }
+  
+  /* distance addition, handling absorption of distances to infinity */
+  private static int FWdplus (int w1, int w2) {
+    int max = Integer.MAX_VALUE;
+    if (w1 == max || w2 == max)
+      return max;
+    if (w2 >= (max - w1))
+      return max;
+    return w1 + w2;
+  }
+  
+  public static <N,E> dmap<N> FloydWarshal (final Graph<N,E> graph) {
+    //int [][] d = new int[graph.size ()][graph.size ()];
+    dmap<N> dmap = new dmap<N> ();
+    
+    for (N k : graph)
+      for (N i : graph)
+        for (N j : graph)
+          dmap.set (i, j, Math.min(FWdist (graph, dmap, i,j),
+                                   FWdplus (FWdist(graph, dmap, i, k), 
+                                            FWdist (graph, dmap, k, j))));
+    return dmap;
+  }
+  
+  public static <N,E> Map<String,Double> stats (final Graph<N,E> graph) {
+    return stats (FloydWarshal (graph), graph);
+  }
+  
+  public static <N,E> Map<String,Double> stats (final dmap<N> dmap, final Graph<N,E> graph) {
+    Map<String, Double> results = new HashMap<String, Double> ();
+    double avg = 0, delta2 = 0;
+    int max = 0, num = 0;
+    int radius = Integer.MAX_VALUE;
+    int diameter = 0;
+    
+    for (Map.Entry<N,Map<N,Integer>> from : dmap.entrySet ()) {
+      int eccentricity = 0;
+      
+      for (Map.Entry<N,Integer> to: from.getValue ().entrySet ()) {
+        int w = to.getValue ();
+        double delta = (double)w - avg;
+        max = Math.max (max, w);
+        
+        debug.printf ("%s -> %s: %d\n",
+                      from.getKey (), to.getKey (), w);
+        
+        if (w == 0)
+          continue;
+        
+        eccentricity = Math.max (eccentricity, w);
+        avg += delta / ++num;
+        delta2 += delta * (w - avg);
+        
+        debug.printf ("w: %d, num %d, avg %4f, delta2 %4f\n",
+                      w, num, avg, delta2);
+      }
+      radius = Math.min (radius, eccentricity);
+      diameter = Math.max (diameter, eccentricity);
+    }
+    
+    double stddev = Math.sqrt (avg /(Math.sqrt (num)));
+    results.put ("max", (double) max);
+    results.put ("avg", avg);
+    results.put ("stddev", stddev);
+    results.put ("stderr",stddev / Math.sqrt(num));
+    results.put ("radius", (double) radius);
+    results.put ("diameter", (double) diameter);
+    
+    return results;
   }
 }
---
+++ src/org/nongnu/multigraph/metrics/dmap.java
@@ -0,0 +1,46 @@
+package org.nongnu.multigraph.metrics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class dmap<N> {
+  private static final long serialVersionUID = 1L;
+  
+  private Map<N, Map<N,Integer>> dmap = new HashMap<N, Map<N,Integer>> ();
+  
+  public int dist (N from, N to) {
+    Map<N,Integer> to_weights;
+    Integer w;
+    
+    if ((to_weights = dmap.get (from)) != null
+        && (w = to_weights.get (to)) != null)
+      return w.intValue ();
+    
+    return Integer.MAX_VALUE;
+  }
+  
+  public void set (N from, N to, int w) {
+    Map<N,Integer> to_weights;
+    Integer oldw;
+    
+    if (w == Integer.MAX_VALUE)
+      return;
+    
+    if (w < 0)
+      throw new IllegalArgumentException ("weight " + w + " is less than 0");
+    
+    if ((to_weights = dmap.get (from)) == null) {
+      to_weights = new HashMap<N,Integer> ();
+      dmap.put (from, to_weights);
+    }
+    
+    if ((oldw = to_weights.get (to)) == null
+        || oldw.intValue () > w)
+      to_weights.put (to, w);
+  }
+  
+  Set<Map.Entry<N,Map<N,Integer>>> entrySet() {
+    return dmap.entrySet ();
+  }
+}
